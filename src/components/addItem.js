3816fcb9f652969f43d75d6db6d98ce5162fa58a
livi <scholtz.ice@gmail.com>
2024-02-10 22:48:21 +0200

    Refeactored async code from then to async await syntax

--- src/components/addItem.js
+++ src/components/addItem.js
@@ -8,58 +8,73 @@ const AddItem = () => {
     const [itemImageToUpload, setItemImageToUpload] = useState(null);
     const [itemImageUrls, setItemImageUrls] = useState([]);
 
+    // ref to dir in firebase storage & giving randomized name to uploaded image
+    const itemImageRef = ref(storage, `images/${itemImageToUpload + v4()}`);
+
     // called when user clicks upload button
-    const uploadImage = (e) => {
+    const uploadImage = async (e) => {
         e.preventDefault();
 
         if (itemImageToUpload === null) return;
-        // ref to dir in firebase storage & giving randomized name to uploaded image
-        const itemImageRef = ref(
-            storage,
-            `images/${itemImageToUpload.name + v4()}`
-        );
-        // after image file has been selected
-        uploadBytes(itemImageRef, itemImageToUpload)
-            .then((snapshot) => {
-                getDownloadURL(snapshot.ref).then((url) => {
-                    setItemImageUrls([...itemImageUrls, { url, key: url }]);
-                });
-            })
-            .catch((err) => {
-                alert(err);
-            });
+
+        try {
+            // after image file has been selected
+            const snapshot = await uploadBytes(itemImageRef, itemImageToUpload);
+            // display image that's just been uploaded
+            const url = getDownloadURL(snapshot.ref);
+            setItemImageUrls([...itemImageUrls, url]);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     // ref to dir in firebase that images were uploaded to
     const itemImagesRef = ref(storage, "images/");
 
     useEffect(() => {
-        // firebase method to get all results of dir of given ref
-        listAll(itemImagesRef)
-            .then((res) => {
-                res.items.forEach((item) => {
-                    getDownloadURL(item).then((url) => {
-                        setItemImageUrls([...itemImageUrls, { url, key: url }]);
-                    });
-                });
-                console.log("items already in storage", res);
-            })
-            .catch((err) => {
-                alert(err);
-            });
+        const getImages = async () => {
+            try {
+                const response = await listAll(itemImagesRef);
+                const downloadedItemImages = response.items.map(
+                    async (item) => {
+                        return await getDownloadURL(item);
+                    }
+                );
+                setItemImageUrls([...itemImageUrls, ...downloadedItemImages]);
+                console.log(itemImageUrls);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        console.log("1", itemImageUrls);
+        getImages();
+        console.log("2", itemImageUrls);
+
+        // })
+        //     .then((res) => {
+        //         res.items.forEach((item) => {
+        //             getDownloadURL(item).then((url) => {
+        //                 setItemImageUrls([...itemImageUrls, url]);
+        //             });
+        //         });
+        //     })
+        //     .catch((err) => {
+        //         alert(err);
+        //     });
     }, []);
 
     return (
         <div>
             <input
                 type="file"
-                onChange={(e) => setItemImageToUpload(e.target.files[0])}
+                onChange={(e) => setItemImageToUpload(e.target.files[0].name)}
             />
             <button onClick={uploadImage}>Upload Image</button>
 
             {/* render item component for each item image url */}
             {itemImageUrls.map((url) => {
-                return <Item itemImg={itemImg} />;
+                return <Item itemImg={url} key={v4()} />;
             })}
         </div>
     );
