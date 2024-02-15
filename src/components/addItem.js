f00e5e0114e1e9ece55f82e038a70b07eb5341e5
livi <scholtz.ice@gmail.com>
2024-02-15 08:03:24 +0200

    Moved functions & state from AddItem to App

--- src/components/addItem.js
+++ src/components/addItem.js
@@ -1,38 +1,7 @@
-import { useState, useEffect, useRef } from "react";
-import Item from "./Item";
-import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
-import { storage } from "../firebase";
-import { v4 } from "uuid";
+import { useRef } from "react";
 
-const AddItem = () => {
+const AddItem = (imageUpload, handleInputChange, uploadFile) => {
     const inputRef = useRef();
-    const [imageUpload, setImageUpload] = useState(null);
-    const [imageUrls, setImageUrls] = useState([]);
-
-    const uploadFile = () => {
-        if (imageUpload == null) return;
-        const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-        uploadBytes(imageRef, imageUpload).then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((url) => {
-                setImageUrls((prev) => [...prev, url]);
-            });
-        });
-    };
-
-    const handleInputChange = (event) => {
-        setImageUpload(event.target.files[0]);
-    };
-
-    useEffect(() => {
-        const imagesListRef = ref(storage, "images/");
-        listAll(imagesListRef).then((response) => {
-            response.items.forEach((item) => {
-                getDownloadURL(item).then((url) => {
-                    setImageUrls((prev) => [...prev, url]);
-                });
-            });
-        });
-    }, []);
 
     return (
         <div>
@@ -58,13 +27,6 @@ const AddItem = () => {
             >
                 Save
             </button>
-
-            {/* List of displayed wardrobe items */}
-            <div className="flex flex-col items-center justify-center">
-                {imageUrls.map((url) => {
-                    return <Item itemImg={url} key={v4()} />;
-                })}
-            </div>
         </div>
     );
 };
